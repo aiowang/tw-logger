f7a7d7b5f0648df805b084da3a8435c15c0fa2cd
aiowang@163.com <aio19851223>
2018-07-18 13:43:50 +0800

    init

---
+++ src/main/java/com/telewave/logger/TwLoggerApplication.java
@@ -0,0 +1,12 @@
+package com.telewave.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwLoggerApplication.class, args);
+	}
+}
